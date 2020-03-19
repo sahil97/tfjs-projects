7fb89df759a345e56a179992783e85d7641f32e5
sahil97 <sahil.sharma071997@gmail.com>
2020-03-19 17:06:49 +0530

    added build

---
+++ object-detection/build/precache-manifest.73bed251da16a7c8bbbc91d548876813.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711d31d49776244479eaaa0cdd96279c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cf45066319b03967b60",
+    "url": "/static/css/main.dc6badfd.chunk.css"
+  },
+  {
+    "revision": "14d00ee0a70a607ad498",
+    "url": "/static/js/2.861b996c.chunk.js"
+  },
+  {
+    "revision": "f98a8e290e152d03115d9d57dc71c8fb",
+    "url": "/static/js/2.861b996c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cf45066319b03967b60",
+    "url": "/static/js/main.56e0b2be.chunk.js"
+  },
+  {
+    "revision": "77bbe6daf4927c3eb37e",
+    "url": "/static/js/runtime-main.60d2814f.js"
+  }
+]);
